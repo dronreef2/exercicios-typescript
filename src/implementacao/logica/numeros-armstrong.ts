89bf79a29055b2e44e9ac8e0aa12d5e88d94468a
{dronreef2} <202001579176@alunos.estacio.br>
2025-04-03 11:55:06 -0300

    envio do numeros

--- src/implementacao/logica/numeros-armstrong.ts
+++ src/implementacao/logica/numeros-armstrong.ts
@@ -1,4 +1,16 @@
-function numerosArmstrong(numero: number) {
-    return null;
+function numerosArmstrong(numero) {
+    const digitos = String(numero).split('');
+    const numDigitos = digitos.length;
+    
+    const soma = digitos.reduce((acc, digito) => {
+        return acc + Math.pow(parseInt(digito), numDigitos);
+    }, 0);
+    
+    if (soma === numero) {
+        return "Este eh um numero de Armstrong!";
+    } else {
+        return "Este nao eh um numero de Armstrong!";
+    }
 }
+
 module.exports = numerosArmstrong;
