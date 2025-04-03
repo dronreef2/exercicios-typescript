fac1c69db8a8ecb1ea1386b93d2ae5b9b44aa0ea
Andréa Leite <andrealeite.pf@gmail.com>
2025-04-03 15:57:54 -0300

    exercicio em andamento

--- src/implementacao/logica/objeto-data.ts
+++ src/implementacao/logica/objeto-data.ts
@@ -1,4 +1,11 @@
 function criaObjeto(data: string) {
+    if (!data || !data.includes('/')) return null;
+
+    const [dia, mes, ano] = data.split ('/').map(Number);
+
+    if (isNaN(dia) || isNaN(mes) || isNaN(ano)) return null;
+    
+
     return null;
 }
 module.exports = criaObjeto;
