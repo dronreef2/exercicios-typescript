8213da832505f4d15c967efb2938c4dc0f8e712d
Andréa Leite <andrealeite.pf@gmail.com>
2025-04-03 11:50:26 -0300

    Exercicio fizzBuzz

--- src/implementacao/logica/fizz-buzz.ts
+++ src/implementacao/logica/fizz-buzz.ts
@@ -1,5 +1,16 @@
-function fizzBuzz(numero: number) {
-    return null;
+function fizzBuzz(numero) {
+    if (numero % 3 === 0 && numero % 5 === 0) {
+        return "FizzBuzz";
+    }
+    else if (numero % 3 === 0) {
+        return "Fizz";
+    }
+    else if (numero % 5 === 0) {
+        return "Buzz";
+    }
+    else {
+        return String(numero);
+    }
 }
 module.exports = fizzBuzz;
 
