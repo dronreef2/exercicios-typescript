ebee308d575208d229b8a2f3e0ae59894b8174dd
{dronreef2} <202001579176@alunos.estacio.br>
2025-04-04 11:34:47 -0300

    somo votacao

--- src/implementacao/logica/soma-dobrada.ts
+++ src/implementacao/logica/soma-dobrada.ts
@@ -1,4 +1,17 @@
-function somaDobrada(umValor: number, outroValor: number) {
-    return null;
+function somaDobrada(umValor, outroValor) {
+    if (umValor < 1 || outroValor < 1) {
+        return -1;
+    }
+
+
+    const soma = umValor + outroValor;
+
+    if (umValor === outroValor) {
+        return soma * 2;
+    }
+
+
+    return soma;
 }
+
 module.exports = somaDobrada;
--- src/implementacao/logica/votacao.ts
+++ src/implementacao/logica/votacao.ts
@@ -1,4 +1,22 @@
-function apuraVotacao(votos: Array<number>) {
-    return null;
+function apuraVotacao(votos) {
+
+    const contagem = {
+        1: 0,
+        2: 0,
+        3: 0
+    };
+
+    for (const voto of votos) {
+        contagem[voto]++;
+    }
+
+    const maxVotos = Math.max(contagem[1], contagem[2], contagem[3]);
+
+    const vencedores = Object.entries(contagem).filter(([_, votos]) => votos === maxVotos).map(([candidato]) => candidato);
+
+    if (vencedores.length > 1) {
+        return 'Empate';
+}
+    return `Vencedor: ${vencedores[0][0]}`;
 }
 module.exports = apuraVotacao;
