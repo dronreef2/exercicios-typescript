07c83b9923ba6adf0b87f26b3944cccc5a14dff3
Andréa Leite <andrealeite.pf@gmail.com>
2025-04-04 10:56:35 -0300

    Atualização exercicio objeto-data

--- src/implementacao/logica/objeto-data.ts
+++ src/implementacao/logica/objeto-data.ts
@@ -1,11 +1,18 @@
 function criaObjeto(data: string) {
+   
     if (!data || !data.includes('/')) return null;
 
-    const [dia, mes, ano] = data.split ('/').map(Number);
+    const [dia, mes, ano] = data.split('/');
 
-    if (isNaN(dia) || isNaN(mes) || isNaN(ano)) return null;
-    
+    if (!dia || !mes || !ano) return null;
 
-    return null;
+    return {
+        dia,
+        mes,
+        ano
+    };
 }
+
 module.exports = criaObjeto;
+
+console.log("12/06/2012");
